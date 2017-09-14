e7a79f3cf6162a7215262f903655a8ecaaf5a5dc
Andrew Marmon <drewmarmon@gmail.com>
2017-09-14 12:10:17 -0400

    Added client stuff.

---
+++ client/client/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const divStyle= {
+  height: 100,
+  width: 100,
+  backgroundColor: "blue"
+}
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="background">
+     </div>
+
+    );
+  }
+}
---
+++ client/client/data/mock-stories.js
@@ -0,0 +1,53 @@
+const stories = [
+  {
+    'name' : '‘Top 10’ graphic misleads about James Comey’s testimony',
+    'link' : 'http://www.politifact.com/truth-o-meter/article/2017/jun/16/top-10-graphic-misleads-about-james-comeys-testimo/',
+    'tags' : [
+      'Comey',
+      'Trump',
+      'hearing',
+      'investigation',
+      'fact'
+    ],
+    'issue' : {
+      'issueId': '5714',
+      'tags' : [
+        'Comey',
+        'Trump',
+        'hearing',
+        'investigation',
+        'fact'
+      ]
+    }
+  },
+  {
+    'name' : 'Trump says he is being investigated over Comey firing',
+    'link' : 'http://www.cnn.com/2017/06/16/politics/donald-trump-james-comey-investigated/index.html',
+    'issue' : {
+      'issueId': '5714',
+      'tags' : [
+        'Comey',
+        'Trump',
+        'hearing',
+        'investigation',
+        'fact'
+      ]
+    }
+  },
+  {
+    'name' : 'Steve Scalise Among 4 Shot at Baseball Field; Suspect Is Dead',
+    'link' : 'https://www.nytimes.com/2017/06/14/us/steve-scalise-congress-shot-alexandria-virginia.html?_r=0',
+    'issue' : {
+      'issueId' : '5717',
+      'tags' : [
+        'Scalise',
+        'shot',
+        'gunman',
+        'baseball',
+        'assault'
+      ]
+    }
+  }
+]
+
+export default stories;
