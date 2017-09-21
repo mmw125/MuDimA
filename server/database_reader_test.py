5dfa7a595d33022f57ff66597bb2e668544610d4
Mark Wiggans <mmw125@gmail.com>
2017-09-20 23:52:05 -0400

    rename tests

--- server/database_reader_test.py
+++ server/database_reader_test.py
@@ -4,9 +4,9 @@ import unittest
 
 
 class DatabaseReaderTest(unittest.TestCase):
-    def get_topics_test(self):
-        self.assertListEqual(["123abc", "456dfg"], database_reader.get_topics().keys())
+    def test_get_topics(self):
+        self.assertEqual({"123abc", "456dfg"}, set(database_reader.get_topics().keys()))
 
-    def get_stories_for_topic_test(self):
+    def test_get_stories_for_topic(self):
         for key in database_reader.get_topics().keys():
             self.assertTrue(database_reader.get_stories_for_topic(key))
