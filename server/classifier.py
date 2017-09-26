9518a780cecb4c5d2bc6d42ff9246976c5e9dbe7
Mark Wiggans <mmw125@gmail.com>
2017-09-26 15:01:59 -0400

    Some baseline code for the classifier.
    The groupings that this code makes should be good enough for now

--- server/classifier.py
+++ server/classifier.py
@@ -1,3 +1,50 @@
+import constants
+import news_fetcher
+
+
+class Grouping(object):
+    """Represents a set of articles that should be about the same topic."""
+    def __init__(self, article):
+        self._articles = [article]
+
+    def add_article(self, article):
+        self._articles.append(article)
+
+    def get_articles(self):
+        return self._articles
+
+    def combine_group(self, group):
+        self._articles.extend(group.get_articles())
+
+    def best_similarity(self, article):
+        return max(article.keyword_similarity(group_article) for group_article in self._articles)
+
+    def __str__(self):
+        return str(article for article in self._articles)
+
 
 def group_articles(article_list):
-    pass
+    """Group articles from the article list into Grouping objects."""
+    article_list = [a if isinstance(a, news_fetcher.Article) else news_fetcher.Article(url=a) for a in article_list]
+    for article in article_list:
+        print article.get_keywords()
+    groupings = []
+    for article in article_list:
+        best_grouping, best_grouping_similarity = None, 0
+
+        # Need to make a shallow copy of list for the possibility of combining two of the items in the list.
+        for grouping in groupings[:]:
+            similarity = grouping.best_similarity(article)
+            if similarity > best_grouping_similarity:
+                # If this article has a high similarity with two separate groups, then combine the groups.
+                if best_grouping_similarity > constants.MIN_COMBINE_GROUP_PERCENTAGE:
+                    grouping.combine_group(best_grouping)
+                    groupings.remove(best_grouping)
+                best_grouping = grouping
+                best_grouping_similarity = similarity
+
+        if best_grouping is not None and best_grouping_similarity > constants.MIN_GROUPING_PERCENTAGE:
+            best_grouping.add_article(article)
+        else:
+            groupings.append(Grouping(article))
+    return groupings
---
+++ server/constants.py
@@ -0,0 +1,7 @@
+# A place for all of those "magic numbers" that may need to be fiddled with.
+
+# There must be this much keyword similarity to attach two articles together.
+MIN_GROUPING_PERCENTAGE = .1
+
+# If two groupings both have this similarity to an article, combine the groups.
+MIN_COMBINE_GROUP_PERCENTAGE = .3
