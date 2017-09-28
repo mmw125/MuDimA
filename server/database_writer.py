ff2dac83d403470dd38a9cb6516214df59707acd
jteves <Jacob.Teves@solers.com>
2017-09-28 16:57:14 -0400

    Adding writer to add list of articles by topic

--- server/database_writer.py
+++ server/database_writer.py
@@ -1,3 +1,23 @@
+import dbconstants
+def db_connect():
+    connection = MySQLdb.connect(host = dbconstants.HOST, user = dbconstants.USER, passwd = dbconstants.PASSWD, db = dbconstants.DB)
+    cursor = connection.cursor()
+    return connection, cursor
 
 def write_topics_to_database(topic_list):
-    pass
+    try:
+        connection, cursor = db_connect()
+        
+        cursor.executemany(
+                """INSERT INTO Article (name, link, summary, actualDate, Cluster_id, Topic_id)
+                VALUES (%s, %s, %s, %s, NULL, %s); """, topic_list)
+        conection.commit()
+
+    except:
+        print "Didn't add news to topic"
+
+    finally:
+        cursor.close()
+        connection.close()
+
+    #pass
