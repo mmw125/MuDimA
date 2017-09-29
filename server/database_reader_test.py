de834835312f8f954eb44ec59169e7ea0b0eb3d6
Andrew Marmon <drewmarmon@gmail.com>
2017-09-29 12:45:07 -0400

    testing stuff.

--- server/database_reader_test.py
+++ server/database_reader_test.py
@@ -5,7 +5,7 @@ import unittest
 
 class DatabaseReaderTest(unittest.TestCase):
     def test_get_topics(self):
-        self.assertEqual({"123abc", "456dfg"}, set([i.id for i in database_reader.get_topics()['topics']]))
+        self.assertEqual({"123abc", "456dfg"}, set([i['id'] for i in database_reader.get_topics()['topics']]))
 
     def test_get_stories_for_topic(self):
         for val in database_reader.get_topics()['topics']:
