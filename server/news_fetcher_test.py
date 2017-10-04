94ee29d135ce059c918c76aeaf9c9b54d1636abb
mshuler <mshuler@vt.edu>
2017-10-04 01:13:41 -0400

    added mocking to the news fetcher test and started tested the authenticity of messages. The top headlines .json file was added to support the news_fetcher test. It was taken from the API and modified.

--- server/news_fetcher_test.py
+++ server/news_fetcher_test.py
@@ -1,11 +1,26 @@
 import news_fetcher
 import unittest
+import mock
+import requests
 
+# This method will be used by the mock to replace requests.get
+def mocked_requests_get(*args, **kwargs):
+    file_object = open("../test/api_top_headlines.json", "r")
+    response = requests.Response()
+    response.raw = file_object
+    return response
 
 class NewsFetcherTest(unittest.TestCase):
-    @staticmethod
-    def test_get_top_headlines():
-        news_fetcher.get_top_headlines()
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    def test_fetch(self, mock_get):
+        self.source =  news_fetcher.get_top_headlines()[0]
+        self.assertEqual(self.source.get_id(), "test")
+        self.assertEqual(self.source.get_name(), "test")
+        self.assertEqual(self.source.get_description(), "test")
+        self.assertEqual(self.source.get_url(), "http://test.test.test")
+        self.assertEqual(self.source.get_category(), "general")
+        self.assertEqual(self.source.get_language(), "en")
+        self.assertEqual(self.source.get_country(), "us")
 
     @staticmethod
     def test_get_sources():
