eca29ff1b4ff7367bda7f15221f97c7ca3094da6
Mark Wiggans <mmw125@gmail.com>
2017-10-04 00:25:24 -0400

    Get database reader working with front end

--- client/client/components/Home.jsx
+++ client/client/components/Home.jsx
@@ -15,7 +15,7 @@ export default class Home extends React.Component {
       .then((responseJson) => {
         this.setState({
           isLoading: false,
-          topics: responseJson.topics
+          topics: responseJson
         })})
       .catch((error) => { console.error(error); });
   }
--- server/database_reader.py
+++ server/database_reader.py
@@ -4,10 +4,15 @@ import database_utils as utils
 def get_topics():
     with utils.DatabaseConnection() as (connection, cursor):
         cursor.execute("SELECT * FROM topic")
-        return cursor.fetchall()
+        return [{"title": item[0], "id": item[1], "image": item[2]} for item in cursor.fetchall()]
 
 
 def get_stories_for_topic(topic_id):
     with utils.DatabaseConnection() as (connection, cursor):
         cursor.execute("SELECT * FROM article WHERE Topic_id=?", topic_id)
         return cursor.fetchall()
+
+if __name__ == "__main__":
+    print get_topics()
+    for topic in get_topics():
+        print get_stories_for_topic(topic.get("id"))
