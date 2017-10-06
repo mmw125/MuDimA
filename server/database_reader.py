5f4f2133489423d8b1a0a84de118ea4a0a44d2bd
Mark Wiggans <mmw125@gmail.com>
2017-10-06 13:38:14 -0400

    Get articles from database in groups with keywords

--- server/database_reader.py
+++ server/database_reader.py
@@ -1,8 +1,8 @@
-import database_utils as utils
+import database_utils
 
 
 def get_topics():
-    with utils.DatabaseConnection() as (connection, cursor):
+    with database_utils.DatabaseConnection() as (connection, cursor):
         cursor.execute("SELECT topic.name, topic.id, topic.image_url, count(*) FROM article, topic "
                        "WHERE article.topic_id = topic.id GROUP BY topic.id;")
         return sorted([{"title": item[0], "id": item[1], "image": item[2], "count": item[3]}
@@ -10,11 +10,28 @@ def get_topics():
 
 
 def get_stories_for_topic(topic_id):
-    with utils.DatabaseConnection() as (connection, cursor):
+    with database_utils.DatabaseConnection() as (connection, cursor):
         cursor.execute("SELECT name FROM topic WHERE id=?", (topic_id,))
         title = cursor.fetchone()[0]
         cursor.execute("SELECT * FROM article WHERE topic_id=?", (topic_id,))
         return {"title": title, "articles": [(item[0], item[1]) for item in cursor.fetchall()]}
+
+
+def get_grouped_articles():
+    with database_utils.DatabaseConnection() as (connection, cursor):
+        cursor.execute("SELECT name, keywords, id, url FROM article")
+        groups = {}
+        for item in cursor.fetchall():
+            name, keywords, id, url = item
+            article = database_utils.Article(url=url, title=name)
+            article.set_keywords(keywords)
+            if id in groups:
+                groups.get(id).add_article(article)
+            else:
+                group = database_utils.Grouping(article)
+                group.set_uuid(id)
+        return groups.values()
+
 
 if __name__ == "__main__":  # pragma: no cover
     print get_topics()
