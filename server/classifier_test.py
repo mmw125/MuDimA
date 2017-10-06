7bdf58c92c982708c01fff0b8776305daf757800
Mark Wiggans <mmw125@gmail.com>
2017-10-06 18:05:15 -0400

    Fix test

--- server/classifier_test.py
+++ server/classifier_test.py
@@ -1,4 +1,5 @@
 import classifier
+import database_utils
 import news_fetcher
 import test_utils
 import unittest
@@ -6,9 +7,9 @@ import unittest
 
 class ClassifierTest(unittest.TestCase):
     def test_similar_urls(self):
-        similar = [news_fetcher.Article(url) for url in test_utils.SIMILAR_URLS]
+        similar = [database_utils.Article(url) for url in test_utils.SIMILAR_URLS]
         self.assertEqual(1, len(classifier.group_articles(test_utils.SIMILAR_URLS)))
 
     def test_dissimilar_urls(self):
-        dissimilar = [news_fetcher.Article(url) for url in test_utils.DISSIMILAR_URLS]
+        dissimilar = [database_utils.Article(url) for url in test_utils.DISSIMILAR_URLS]
         self.assertEqual(2, len(classifier.group_articles(test_utils.DISSIMILAR_URLS)))
