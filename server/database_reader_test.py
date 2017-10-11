19bb037dcc1ccbe860c0a075cf703c1a5ef4fdcd
Mark Wiggans <mmw125@gmail.com>
2017-10-11 19:43:10 -0400

    Add test for getting urls

--- server/database_reader_test.py
+++ server/database_reader_test.py
@@ -11,3 +11,9 @@ class DatabaseReaderTest(test_utils.DatabaseTest):
         stories = database_reader.get_stories_for_topic(groups[0].get_uuid())
         stories = set(a[1] for a in stories.get('articles'))
         self.assertEqual(stories, set(model.get_url() for model in test_utils.SIMILAR_ARTICLES))
+
+    def test_get_urls(self):
+        self.assertEqual(set(database_reader.get_urls()), set())
+        groups = classifier.group_articles(test_utils.SIMILAR_ARTICLES)
+        database_writer.write_topics_to_database(groups)
+        self.assertEqual(set(database_reader.get_urls()), set(model.get_url() for model in test_utils.SIMILAR_ARTICLES))
