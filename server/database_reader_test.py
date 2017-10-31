128b191936d0fef78d3f735e18e706455d59f0a4
Mark Wiggans <mmw125@gmail.com>
2017-10-31 17:12:09 -0400

    Return a dict for each story

--- server/database_reader_test.py
+++ server/database_reader_test.py
@@ -15,7 +15,7 @@ class DatabaseReaderTest(test_utils.DatabaseTest):
         groups = classifier.group_articles(test_utils.SIMILAR_ARTICLES)
         database_writer.write_topics_to_database(groups)
         stories = database_reader.get_stories_for_topic(groups[0].get_uuid())
-        stories = set(a[1] for a in stories.get('articles'))
+        stories = set(a.get('url') for a in stories.get('articles'))
         self.assertEqual(stories, set(model.get_url() for model in test_utils.SIMILAR_ARTICLES))
         self.assertEqual(1, database_reader.get_number_topics())
 
