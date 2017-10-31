ceb33cf0b19cb7bf9d7f187808087914a1de7bb7
Mark Wiggans <mmw125@gmail.com>
2017-10-31 01:09:18 -0400

    Add progress statements when populating database

--- server/news_fetcher.py
+++ server/news_fetcher.py
@@ -74,5 +74,5 @@ def update_database():
     articles = get_top_headlines(add_category_information=True)
     urls_in_database = database_reader.get_urls()
     articles = [article for article in articles if article.get_url() not in urls_in_database]
-    grouped = classifier.group_articles(articles)
+    grouped = classifier.group_articles(articles, debug=True)
     database_writer.write_topics_to_database(grouped)
