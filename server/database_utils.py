49c4e63687479198b15c53e0abdc427bd61fc68b
Mark Wiggans <mmw125@gmail.com>
2017-11-11 01:49:16 -0500

    Add new columns to database

--- server/database_utils.py
+++ server/database_utils.py
@@ -29,10 +29,12 @@ class DatabaseConnection:
             self._create_tables()
 
     def _create_tables(self):
-        self.cursor.execute("""CREATE TABLE topic (name TEXT, id TEXT PRIMARY KEY, image_url TEXT, category TEXT)""")
+        self.cursor.execute("""CREATE TABLE topic (name TEXT, id TEXT PRIMARY KEY, image_url TEXT, 
+                               category TEXT, fit_x DOUBLE, fit_y DOUBLE)""")
         self.cursor.execute("""CREATE TABLE article (name TEXT, link TEXT PRIMARY KEY, image_url TEXT,
-                               article_text TEXT, keywords TEXT, topic_id TEXT, date DATETIME, fit_x DOUBLE,
-                               popularity UNSIGNED INT, fit_y DOUBLE,
+                               article_text TEXT, keywords TEXT, topic_id TEXT, date DATETIME,
+                               fit_x DOUBLE, fit_y DOUBLE, group_fit_x DOUBLE, group_fit_y DOUBLE,
+                               popularity UNSIGNED INT DEFAULT 1,
                                FOREIGN KEY(topic_id) REFERENCES topic(id) ON DELETE CASCADE)""")
         self.connection.commit()
 
