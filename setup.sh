7be81a331ae5ca765b30f3e8ec5b0dc73fb0ab4b
EC2 Default User <ec2-user@ip-172-31-29-235.us-east-2.compute.internal>
2017-11-20 21:09:45 +0000

    script and change of server host for deployment

---
+++ setup.sh
@@ -0,0 +1,19 @@
+Collecting Flask (from -r server/environments/requirements.txt (line 1))
+  Downloading Flask-0.12.2-py2.py3-none-any.whl (83kB)
+Collecting flask-cors (from -r server/environments/requirements.txt (line 2))
+  Downloading Flask_Cors-3.0.3-py2.py3-none-any.whl
+Collecting nltk>=3.2.1 (from -r server/environments/requirements.txt (line 3))
+  Downloading nltk-3.2.5.tar.gz (1.2MB)
+Collecting newspaper (from -r server/environments/requirements.txt (line 4))
+  Downloading newspaper-0.1.0.7.tar.gz (176kB)
+Requirement already satisfied: python-dateutil in /usr/lib/python2.7/dist-packages (from -r server/environments/requirements.txt (line 5))
+Requirement already satisfied: requests in /usr/lib/python2.7/dist-packages (from -r server/environments/requirements.txt (line 6))
+Collecting scikit-learn[alldeps] (from -r server/environments/requirements.txt (line 7))
+  Downloading scikit_learn-0.19.1-cp27-cp27mu-manylinux1_x86_64.whl (12.2MB)
+pip install -r server/environments/requirements.txt
+pip install -r server/environments/requirements.txt
+ pip install server/environments/requirements-test.txt
+pip install coveralls
+
+python -m nltk.downloader all
+
